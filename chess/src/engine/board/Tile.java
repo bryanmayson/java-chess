fbf87cf931b8c15356672085797d9629b63c7a5a
bmayson <bmayson@bmayson>
2021-02-24 14:37:50 +0700

    reinforced immutability of the classes and added dependencies to guava

--- chess/src/engine/board/Tile.java
+++ chess/src/engine/board/Tile.java
@@ -2,17 +2,38 @@ package engine.board;
 
 import engine.pieces.Piece;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
+
 /**
  * @author bmayson
  *
  */
 public abstract class Tile {
 
-	int tileCoordinate;
+	protected final int tileCoordinate;
+	
+	private static final Map<Integer,EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+	
+	
+	private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+		final Map<Integer,EmptyTile> emptyTileMap = new HashMap<>();
+		
+		for(int i = 0 ; i<64;i++) {
+			emptyTileMap.put(i,new EmptyTile(i));
+		}
+		
+		return ImmutableMap.copyOf(emptyTileMap);
+	}
+
 	
 	public Tile(int tileCoordinate) {
 		this.tileCoordinate = tileCoordinate;
 	}
+
 
 	public abstract boolean isTileOccupied();
 	
@@ -37,7 +58,7 @@ public abstract class Tile {
 	
 	/*OccupiedTile subclass*/
 	public static final class OccupiedTile extends Tile{
-		Piece pieceOnTile;
+		private final Piece pieceOnTile;
 		
 		public OccupiedTile(int coordinate, Piece pieceOnTile) {
 			super(coordinate);
--- chess/src/module-info.java
+++ chess/src/module-info.java
@@ -1,2 +1,3 @@
 module chess {
+	requires guava;
 }
