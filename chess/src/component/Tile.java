d3d2b55911cfe4fed0bdb0f934675967f6d23a77
bmayson <bmayson@bmayson>
2021-02-24 13:47:53 +0700

    Implemented a Tile class for chess

---
+++ chess/src/component/Tile.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package component;
+
+/**
+ * @author bmayson
+ *
+ */
+public abstract class Tile {
+
+	int tileCoordinate;
+	
+	public Tile(int tileCoordinate) {
+		this.tileCoordinate = tileCoordinate;
+	}
+
+	public abstract boolean isTileOccupied();
+	
+	public abstract Piece getPiece();
+	
+	
+	/*EmptyTile subclass*/
+	public static final class EmptyTile extends Tile{
+		public EmptyTile(int coordinate) {
+			super(coordinate);
+		}
+		@Override
+		public boolean isTileOccupied() {
+			return false;
+		}
+		@Override
+		public Piece getPiece() {
+			return null;
+		}
+		
+	}
+	
+	/*OccupiedTile subclass*/
+	public static final class OccupiedTile extends Tile{
+		Piece pieceOnTile;
+		
+		public OccupiedTile(int coordinate, Piece pieceOnTile) {
+			super(coordinate);
+			this.pieceOnTile = pieceOnTile;
+		}
+		
+		@Override
+		public boolean isTileOccupied() {
+			return true;
+		}
+		
+		@Override
+		public Piece getPiece() {
+			return this.pieceOnTile;
+		}
+		
+	}
+	
+}
